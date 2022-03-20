22b42dfc1a315b2f4f8eb6d35db8e211d0a46327
dbsalazarr <dbsalazarr@gmail.com>
2022-03-20 18:50:10 -0500

    Menu Footer terminado

--- footer.php
+++ footer.php
@@ -1,5 +1,16 @@
 	<footer>
-		<p class="text-center">Derecho reservados Andean Trip Machu Picchu</p>
+		<div class="seccion content-menu-footer">
+			<p>Copyright © <?php echo date("Y"); ?> Andean Trip. All rights reserved</p>
+			<?php 
+				$menu_footer = array(
+					'theme_location' => 'menu_principal',
+					'container' => 'nav',
+					'container_class' => 'menu menu-footer',
+					'container_id' => 'menu-footer'
+				);
+				wp_nav_menu($menu_footer);
+			 ?>
+		</div>
 	</footer>
 	<?php wp_footer(); ?>
 	</div>
