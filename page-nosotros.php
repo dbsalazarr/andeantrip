52051b07b7997427742ee1d4e89954e8f0d20a41
dbsalazarr <dbsalazarr@gmail.com>
2022-03-21 23:16:38 -0500

    Termino con la parte de servicios terminados

--- page-nosotros.php
+++ page-nosotros.php
@@ -1,8 +1,32 @@
 <?php get_header('page'); ?>
-	<section class="seccion contenido-nosotros contenido-tours">
+	<!-- DESCRIPCIÓN DE LA EMPRESA -->
+	<section class="seccion content-nosotros contenido-tours">
 		<?php the_content(); ?>
 	</section>
+	<!-- FIN DESCRIPCIÓN DE LA EMPRESA -->
+	<section class="seccion content-nosotros detalles-servicios">
+		<h3>SOBRE NOSOTROS</h3>
+		<h1>Detalles de nuestros servicios</h1>
+		<div class="nuestros-servicios">
+			<?php 
+				$html_mark = '<article class="item-servicios">';
+				$field = "";
+				for ($i=1; $i <= 4; $i++) { 
+					$field = get_field('detalles_servicios_'.$i);
+					var_dump($field);
+					$html_mark .= '<span class="icon-servicios"> icon-'. $i .'</span>';
+					$html_mark .= '<h3 class="title-servicios">' .$field['titulo_servicio']. '</h3>';
+					$html_mark .= '<p>'. $field['descripcion_servicio'].'</p>';
+				}
+
+				$html_mark .= '</article>';
+				echo $html_mark;
+			?>
+		</div>
+	</section>
+	<!-- ULTIMOS TEMPLATES -->
 	<section class="seccion">
 		<?php get_template_part('templates/ultimos', 'viajes'); ?>
 	</section>
+	<!-- FIN ULTIMOS TEMPLATES -->
 <?php get_footer(); ?>
