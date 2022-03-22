6d33dd5189c5ec5d0044b7f1f781b346b6225aa0
dbsalazarr <dbsalazarr@gmail.com>
2022-03-21 23:27:49 -0500

    Avances de nuestros servicios
    Falta cargar una imagen de fondo

--- page-nosotros.php
+++ page-nosotros.php
@@ -1,29 +1,31 @@
-<?php get_header('page'); ?>
+	<?php get_header('page'); ?>
 	<!-- DESCRIPCIÓN DE LA EMPRESA -->
 	<section class="seccion content-nosotros contenido-tours">
 		<?php the_content(); ?>
 	</section>
 	<!-- FIN DESCRIPCIÓN DE LA EMPRESA -->
+
+	<!-- NUESTROS SERVICIOS -->
 	<section class="seccion content-nosotros detalles-servicios">
 		<h3>SOBRE NOSOTROS</h3>
 		<h1>Detalles de nuestros servicios</h1>
 		<div class="nuestros-servicios">
 			<?php 
-				$html_mark = '<article class="item-servicios">';
-				$field = "";
+				$html_mark = '';
 				for ($i=1; $i <= 4; $i++) { 
 					$field = get_field('detalles_servicios_'.$i);
-					var_dump($field);
+					$html_mark .= '<article class="item-servicio">';
 					$html_mark .= '<span class="icon-servicios"> icon-'. $i .'</span>';
-					$html_mark .= '<h3 class="title-servicios">' .$field['titulo_servicio']. '</h3>';
+					$html_mark .= '<h4 class="title-servicios">' .$field['titulo_servicio']. '</h4>';
 					$html_mark .= '<p>'. $field['descripcion_servicio'].'</p>';
+					$html_mark .= '</article>';
 				}
-
-				$html_mark .= '</article>';
 				echo $html_mark;
 			?>
 		</div>
 	</section>
+	<!-- FIN DE NUESTROS SERVICIOS -->
+	
 	<!-- ULTIMOS TEMPLATES -->
 	<section class="seccion">
 		<?php get_template_part('templates/ultimos', 'viajes'); ?>
