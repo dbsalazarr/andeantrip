fe7d5b64442c87a3f529d34003b1fb668305a4c8
dbsalazarr <dbsalazarr@gmail.com>
2022-03-21 20:45:51 -0500

    Creada la carpeta template y un archivo para ultimos viajes

--- page-nosotros.php
+++ page-nosotros.php
@@ -1,3 +1,8 @@
 <?php get_header('page'); ?>
+	<section class="seccion contenido">
 
+	</section>
+	<section class="seccion">
+		<?php get_template_part('templates/ultimos', 'viajes'); ?>
+	</section>
 <?php get_footer(); ?>
