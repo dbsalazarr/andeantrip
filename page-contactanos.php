04a21fc2319845c1d4795265b005deec1eb969a1
dbsalazarr <dbsalazarr@gmail.com>
2022-04-01 18:54:37 -0500

    Mapa de google agregado

--- page-contactanos.php
+++ page-contactanos.php
@@ -1,12 +1,15 @@
 <?php get_header('page'); ?>
-	
-	<section class="seccion ubicanos">
-			Mapa de nuestras oficinas
+	<section class="seccion nuestra-ubicacion">
+		<?php the_content(); ?>
 	</section>
-	<section class="seccion contactanos-form">
+	<section class="seccion content-nosotros contact-form">
+		<h3>Contactos</h3>
+		<h1>Contactanos para <br /> Brindarte mas información</h1>
 		<?php 
 			echo do_shortcode(' [contact-form-7 id="109" title="Contact form 1"] ');
 		?>
 	</section>
-
+	<section class="seccion">
+			New content to add
+	</section>
 <?php get_footer(); ?>
