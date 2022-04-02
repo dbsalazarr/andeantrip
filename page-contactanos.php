337594822b459d853085788a633a46c8671decf5
dbsalazarr <dbsalazarr@gmail.com>
2022-04-01 20:21:12 -0500

    Formulario de contacto terminado

--- page-contactanos.php
+++ page-contactanos.php
@@ -2,8 +2,8 @@
 	<section class="seccion nuestra-ubicacion">
 		<?php the_content(); ?>
 	</section>
-	<section class="seccion content-nosotros contact-form">
-		<h3>Contactos</h3>
+	<section class="seccion content-nosotros">
+		<h3>Contacto</h3>
 		<h1>Contactanos para <br /> Brindarte mas información</h1>
 		<?php 
 			echo do_shortcode(' [contact-form-7 id="109" title="Contact form 1"] ');
