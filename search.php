8a2c8bfbc642b9894f6832f3a0fb6f41909ae32c
dbsalazarr <dbsalazarr@gmail.com>
2022-04-09 18:12:26 -0500

    Página sobre los resultados de busqueda terminada

--- search.php
+++ search.php
@@ -3,12 +3,6 @@
 	<?php
 		$destino = $_GET['destino'];
 	?> 
-
-	<?php 
-		get_search_form();
-	?>
-	<h3>Destinos disponibles</h3>
-
 	<?php
 		# * REALIZANDO LA CONSULTA CON LA OPCIÓN DEL WP_Query
 		$args = array(
@@ -22,15 +16,33 @@
 		$busqueda = new WP_Query($args);
 
 	?> 
-	
-	<section class="">
-			<?php while( $busqueda->have_posts() ) : $busqueda->the_post(); ?>
 
-			<article>
-				<h2> <?php the_title(); ?> </h2>
+	<section class="seccion content-nosotros nuestros-tours">
+		<h3>Destinos disponibles</h3>
+		<h1>Explore nuestros <br>Tours</h1>
+		<div class="tours-content">
+			<?php 
+				while( $busqueda->have_posts() ): $busqueda->the_post();
+			?>
+			<article class="tour">
+				<?php the_post_thumbnail('blog'); ?>
+				<div class="descripcion-tour">
+					<span> Cusco </span>
+					<h3> <?php the_title(); ?> </h3>
+					<p>
+						<?php 
+							$excerpt = get_the_excerpt(); 
+							echo cut_text_for_words($excerpt, 20);
+						?>
+					</p>
+
+					<a href="<?php the_permalink(); ?> " class="boton primario"> DESCUBRIR MÁS </a>
+
+				</div>
+
 			</article>
-			
-			<?php endwhile; wp_reset_postdata(); ?>
-	</section> <!-- END TOUR GRID -->
 
+			<?php endwhile; wp_reset_postdata(); ?>
+		</div>
+	</section>
 <?php get_footer(); ?>
