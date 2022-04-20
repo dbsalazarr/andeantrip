bd7a9754dcd49960ddc1b480e820e7ebbf2116d9
dbsalazarr <dbsalazarr@gmail.com>
2022-04-19 20:14:09 -0500

    Template para mostrar destinos deseados terminado

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<h3> Este es el index más simple que veras en tu vida profesional papa :v</h3>
+<?php get_header('page'); ?>
+	<section class="seccion">
+		<h2> No debiste llegar a este lugar, por favor retorna la inicio</h2>
+		<p>Presiona el logo de la web o haz click <a href=" <?php esc_url( blog_info("url") ); ?> ">aquí</a> </p>
+	</section>
+<?php get_footer(); ?>
