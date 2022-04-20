b465d0be53dd7e8a84d125e1603ca1f5066b6dd7
dbsalazarr <dbsalazarr@gmail.com>
2022-04-19 19:52:58 -0500

    Filtrando post para no mostrar post repetidos en una misma entrada

--- home.php
+++ home.php
@@ -1,10 +1,27 @@
 <?php get_header('page'); ?>
-	<section class="seccion content-nosotros nuestros-tours">
+	<section class="seccion content-post">
+
+		<h2 class="title-post"> <?php the_title(); ?> </h2>
+		
+		<div class="blog-seccion-content">
+			<?php the_content(); ?>
+		</div>
+	</section>
+	<?php $actual_id_post = get_the_ID(); ?>
+	<section class="seccion content-nosotros content-blog">
 		<h3>Blog</h3>
 		<h1>Enterate de más tips de <br>Viaje</h1>
 		<div class="tours-content">
 			<?php 
-				while( have_posts() ): the_post();
+				$args = array(
+					'post_type' => 'post',
+					'post__not_in' => array( $actual_id_post )
+				);
+
+				$filter_post = new WP_Query($args);
+			?>
+			<?php 
+				while( $filter_post->have_posts() ): $filter_post->the_post();
 			?>
 			<article class="tour">
 				<?php the_post_thumbnail('blog'); ?>
@@ -18,7 +35,7 @@
 						?>
 					</p>
 
-					<a href="<?php the_permalink(); ?> " class="boton primario"> DESCUBRIR MÁS </a>
+					<a href="<?php the_permalink(); ?> " class="square-button"> <img src="<?php echo get_template_directory_uri(); ?>/media/icon-arrow-right.png" alt="icon arrow right"></a>
 
 				</div>
 
